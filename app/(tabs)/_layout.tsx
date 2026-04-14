54e38805558089c5287393f379457406b207ba67
Nikolai Pedersen Olsen <Nikolai.polsen@gmail.com>
2026-04-14 10:50:50 +0200

    feat: refactor navigation structure to use stack navigator and authentication context

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,53 +1,31 @@
-/**
- * Configures the main tab navigator for the app's primary routes.
- */
-
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/ui/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/themes/colors';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useAuthContext } from '@/hooks/use-auth-context';
+import AuthProvider from '@/providers/auth-provider';
+import { Redirect, Stack } from 'expo-router';
+import { useColorScheme } from 'react-native';
 
-export default function TabLayout() {
+export function RootNavigation() {
+  const { isLoggedIn, isLoading } = useAuthContext();
+
+  if (isLoading) return null;
+
+  if (!isLoggedIn) {
+    return <Redirect href={"/(auth)/start"}/>;
+  }
+  if (isLoggedIn) {
+    //return <Redirect href={"/(tabs)/home"}/>;
+  }
+}
+export default function RootLayout() {
   const colorScheme = useColorScheme();
-
+  const themeColors = Colors[colorScheme ?? 'light'];
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="swipe"
-        options={{
-          title: 'Swipe',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="hand.thumbsup.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="recommendations"
-        options={{
-          title: 'Matches',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="sparkles" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+    <AuthProvider>
+        <Stack>
+          <Stack.Screen name='(auth)' options={{ headerShown: false }}/>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
+        </Stack>
+        <RootNavigation/>
+    </AuthProvider>
   );
 }
