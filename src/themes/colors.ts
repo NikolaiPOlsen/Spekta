12f102f283a2d56a592dcf674dda163f8af1efc7
Nikolai Pedersen Olsen <Nikolai.polsen@gmail.com>
2026-04-14 10:51:09 +0200

    feat: update color theme to include border and primary color for light and dark modes

--- src/themes/colors.ts
+++ src/themes/colors.ts
@@ -11,18 +11,22 @@ export const Colors = {
   light: {
     text: '#11181C',
     background: '#fff',
+    border: '#11181C',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
+    primary: tintColorLight,
   },
   dark: {
     text: '#ECEDEE',
     background: '#151718',
+    border: '#ECEDEE',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
+    primary: tintColorDark,
   },
 };
 
