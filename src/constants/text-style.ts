099297ec8246f0a263fc485b103db2b7487bf699
Nikolai Pedersen Olsen <Nikolai.polsen@gmail.com>
2026-04-21 22:50:41 +0200

    Created text-style constant

---
+++ src/constants/text-style.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const TextStyles = StyleSheet.create ({
+    sectionTitle: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    sectionSubTitle: {
+        fontSize: 16,
+        marginBottom: 15,
+        textAlign: 'center',
+    },
+    inputLabel: {
+        fontSize: 13,
+        fontWeight: '600',
+        marginBottom: 6,
+        marginLeft: 2,
+    },
+})
