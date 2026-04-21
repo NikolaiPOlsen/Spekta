dc6dbca79f8ea3a712e022f3ed289c908259c731
Nikolai Pedersen Olsen <Nikolai.polsen@gmail.com>
2026-04-21 22:50:15 +0200

    Updated profile page to include name, email, password and buttons

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,3 +1,59 @@
+import { InputField, PasswordField } from "@/components/ui/input-field";
+import { useState } from "react";
+import { View, StyleSheet, Text, useWindowDimensions, KeyboardAvoidingView, Platform } from 'react-native';
+import { TextStyles } from '@/constants/text-style';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { AppButton, DeleteButton } from "@/components/ui/app-button";
+
 export default function ProfileRoute() {
-  return null;
+  const { width, height } = useWindowDimensions();
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmpassword, setConfirmPassword] = useState('');
+
+  return (
+      <KeyboardAvoidingView 
+        style={{ flex: 1, width: '100%' }}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'} 
+        keyboardVerticalOffset={0}>
+
+        <View style={styles.container}>
+          <View style={styles.inner}>
+            <Text style={TextStyles.sectionTitle}>Profile</Text>
+
+            <Text style={TextStyles.inputLabel}>Displayname</Text>
+            <InputField name='Displayname' value={username} onChange={setUsername}/>
+
+            <Text style={TextStyles.inputLabel}>Email</Text>
+            <InputField name='Email' value={email} onChange={setEmail} />
+
+            <Text style={TextStyles.inputLabel}>Password</Text>
+            <PasswordField name='Password' value={password} onChange={setPassword}/>
+
+            <Text style={TextStyles.inputLabel}>Confirm password</Text>
+            <PasswordField name='Confirm password' value={confirmpassword} onChange={setConfirmPassword}/>
+          </View>
+
+          {(username || email || password || confirmpassword) && (
+            <AppButton label='Save changes' onPress={() => false}/>
+          )}
+          
+          <AppButton label='Logout' onPress={() => false}/>
+        </View>
+      </KeyboardAvoidingView>
+  )
 }
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inner: {
+    width: '90%',
+  },
+})
