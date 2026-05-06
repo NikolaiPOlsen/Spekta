555b577d13f0c2d221f65cd875f1d6adcaee90f2
Nikolai Pedersen Olsen <Nikolai.polsen@gmail.com>
2026-05-06 12:34:10 +0100

    feat: enhance Swipe component to support swipe callbacks in renderCard

--- src/features/swipe/components/swipe.tsx
+++ src/features/swipe/components/swipe.tsx
@@ -4,25 +4,28 @@ import React, { useRef } from 'react';
 import { useWindowDimensions } from 'react-native';
 
 type SwipeProps<T> = {
-  data: T[];
-  renderCard: (item: T) => React.ReactNode;
-  onSwipeRight?: (item: T, index: number) => void;
-  onSwipeLeft?: (item: T, index: number) => void;
+    data: T[];
+    renderCard: (item: T, swipeLeft: () => void, swipeRight: () => void) => React.ReactNode;
+    onSwipeRight?: (item: T, index: number) => void;
+    onSwipeLeft?: (item: T, index: number) => void;
 };
 
 export function Swipe<T>({ data, renderCard, onSwipeLeft, onSwipeRight }: SwipeProps<T>) {
     const ref = useRef<SwiperCardRefType | null>(null);
 
     const { width, height } = useWindowDimensions();
-    const cardWidth = width * 0.92;
-    const cardHeight = height * 0.80;
+    const cardWidth = width * 1;
+    const cardHeight = height * 1;
+
+    const swipeLeft = () => ref.current?.swipeLeft?.();
+    const swipeRight = () => ref.current?.swipeRight?.();
 
     return (
         <GestureHandlerRootView style={{ width: cardWidth, height: cardHeight }}>
             <Swiper
                 ref={ref}
                 data={data}
-                renderCard={renderCard}
+                renderCard={(item) => renderCard(item, swipeLeft, swipeRight)}
                 cardStyle={{ width: cardWidth, height: cardHeight }}
                 onSwipeRight={(index: number) => onSwipeRight?.(data[index], index)}
                 onSwipeLeft={(index: number) => onSwipeLeft?.(data[index], index)}
